4582e9165dddbb2d5501e22c4064810591dcbf7b
Pedro Reis <pedropcr.42@gmail.com>
2021-08-30 16:54:27 -0300

    classe Usuario

--- index.php
+++ index.php
@@ -2,13 +2,9 @@
 
 require_once("config.php");
 
-$sql = new Sql();
+$root = new Usuario();
 
-$usuarios = $sql->select("
-    SELECT *
-    FROM tb_usuarios
-");
-
-echo json_encode($usuarios);
+$root->loadById(2);
+echo $root;
 
 ?>
